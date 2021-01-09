cb85aa78175f9079e19f2239b5e6a30b3f24a594
Gabriel Masson <gmasson@users.noreply.github.com>
2021-01-09 12:36:52 -0300

    Update index.php

--- index.php
+++ index.php
@@ -7,17 +7,15 @@
 */
 
 /* Aqui você insere a URL */
-$url = "https://copypronta.com/";
+$url = "sua_URL_aqui";
 
 /*
 	Não alterar nada a partir daqui!
 */
 
-header("Access-Control-Allow-Origin: *");
-
 function clonePage( $site_url ){
 	$ch = curl_init();
-	$timeout = 5; // set to zero for no timeout
+	$timeout = 5;
 	curl_setopt ($ch, CURLOPT_URL, $site_url);
 	curl_setopt ($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
 	ob_start();
@@ -33,10 +31,14 @@ echo $page;
 
 /*
 	Substituindo textos
+	Caso precise substituir textos dentro da página, apague o trecho "echo $page;" acima e as marcações de comentários das linhas abaixo.
+	Após isso, insira nas linhas dentro da array, os textos que precisam ser alterados, como no exemplo.
 */
+
 /*
 $changedPage = strtr($page, array(
-    "Texto Original" => "Texto Editado",
+    "Texto Original 1" => "Texto Editado 1",
+    "Texto Original 2" => "Texto Editado 2",
 ));
 echo $changedPage;
 */
